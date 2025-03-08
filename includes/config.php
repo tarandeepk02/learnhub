78221dd6e9c9d6a37075ffc5641ed1723d1d5e40
The-Tarandeep-Kaur <115900559+tarandeepk02@users.noreply.github.com>
2025-03-08 15:10:26 -0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,3 +1,3 @@
 <?php
-$d = mysqli_connect("localhost","root","","learnhubpro")or die(mysqli_connect_error($d));
+$d = mysqli_connect("localhost","root","","learnhub")or die(mysqli_connect_error($d));
 ?>
